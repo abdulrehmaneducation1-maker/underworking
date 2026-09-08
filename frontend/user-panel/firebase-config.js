85174fcf8ffbe8b38cbc8a4cf29c8d37a781b39b
ABDUL REHMAN <abdulrehmaneducation1@gmail.com>
2026-09-09 00:56:52 +0500

    Update Firebase authDomain in configuration

--- frontend/user-panel/firebase-config.js
+++ frontend/user-panel/firebase-config.js
@@ -18,7 +18,7 @@ import { getStorage } from
 
 const firebaseConfig = {
     apiKey: "AIzaSyCgpGk9aHFdVeGoE2rwiUPPWSkfQmTvuIo",
-    authDomain: "nextwave-commerce-project.firebaseapp.com",
+authDomain: "nextwavecommerce.site",
     projectId: "nextwave-commerce-project",
     storageBucket: "nextwave-commerce-project.firebasestorage.app",
     messagingSenderId: "599878226058",
